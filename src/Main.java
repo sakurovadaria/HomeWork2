4a07a7e47cefddbb4a2b216bb093421ac08a6f53
homework <Sakurovadaria@gmail.com>
2024-07-30 21:26:59 +0500

    added gitignore

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,93 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println("Задача 1");
+        System.out.println(cat);
+        System.out.println(dog);
+        System.out.println(paper);
+
+        dog = dog + 4;
+        cat = cat + 4;
+        paper = paper + 4;
+        System.out.println("Задача 2");
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+        dog = dog - 3.5;
+        cat = cat - 1.6;
+        paper = paper - 7639;
+        System.out.println("Задача 3");
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+        var friend = 19;
+        System.out.println("Задача 4");
+        System.out.println(friend);
+        friend = friend +2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+
+        var frog = 3.5;
+        System.out.println("Задача 5");
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+
+        var oneM = 78.2;
+        var twoM = 82.7;
+        System.out.println("Задача 6");
+        System.out.println(oneM);
+        System.out.println(twoM);
+        var totalMass = oneM + twoM;
+        var massDifference = oneM - twoM;
+        System.out.println(totalMass);
+        System.out.println(massDifference);
+
+        System.out.println("Задача 7");
+        var remainderMass = twoM % oneM ;
+        System.out.println("Разница на " + remainderMass + "кг");
+
+        System.out.println("Задача 8.1");
+        var hours = 640;
+        var people = hours / 8;
+        System.out.println("Всего работников в компании —  " + people + " человек");
+
+        System.out.println("Задача 8.2");
+        people = people + 94;
+        hours = people * 8;
+        System.out.println("Если в компании работает " + people + " человек, то всего " + hours + " часов работы может быть поделено между сотрудниками");
+
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
